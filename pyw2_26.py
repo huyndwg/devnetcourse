3650282889d44a649757347d827fe82a392d2f54
Huy Dang Nguyen <huy.ndwg@gmail.com>
2022-10-05 19:32:40 +0700

    x

---
+++ pyw2_26.py
@@ -0,0 +1,7 @@
+a,b,c=map(int,input('Nhap a, b, c: ').split(','))
+if c-b == b-a:
+    print('AP sequence. Next number: {}'.format(c+c-b))
+elif c/b == b/a:
+    print('GP sequence. Next number: {}'.format(c*c/b))
+else:
+    print('Not AP and GP sequence')
