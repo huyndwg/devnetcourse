69206c92c335cf0cc51ba3de0ee5ccee20c057a5
Huy Dang Nguyen <huy.ndwg@gmail.com>
2022-10-06 19:42:41 +0700

    yup

--- a.py
+++ a.py
@@ -1,43 +1,59 @@
-n = int(input('Nhap so: '))
-#so = 1
-sum = 0
 
-# print('Cac so <= {}:'.format(n),end=' ')
-# while so <= n:
-#     if so%5 == 0:
-#         print(so, end =' ')
-#     so = so + 1
+# n = int(input('Nhap so: '))
+# #so = 1
+# sum = 0
 
-# while so <= n:
-#     sum = sum + so
-#     so = so + 1
-# print('Tong: {}'.format(sum))
+# # print('Cac so <= {}:'.format(n),end=' ')
+# # while so <= n:
+# #     if so%5 == 0:
+# #         print(so, end =' ')
+# #     so = so + 1
 
-# while so < n:
-#     if so%2 == 0:
-#         sum = sum + so
-#     so = so + 1
-# print(sum)
+# # while so <= n:
+# #     sum = sum + so
+# #     so = so + 1
+# # print('Tong: {}'.format(sum))
 
-# while so < n:
-#     if n%so == 0 and so%2 == 0:
-#         sum = sum + so
-#     so = so + 1
-# print(sum) 
+# # while so < n:
+# #     if so%2 == 0:
+# #         sum = sum + so
+# #     so = so + 1
+# # print(sum)
 
-so = n-1
-# maxchan = 0
-# while so < n:
-#     if so%2 == 0:
-#         maxchan = so
-#         break
-#     so = so - 1
-# print(maxchan)
+# # while so < n:
+# #     if n%so == 0 and so%2 == 0:
+# #         sum = sum + so
+# #     so = so + 1
+# # print(sum) 
 
-maxle = 1
-while so < n:
-    if n%so == 0 and so%2 ==1:
-        maxle = so
-        break
-    so = so - 1
-print(maxle)
+# so = n-1
+
+# # while so < n:
+# #     if so%2 == 0:
+# #         print(so)
+# #         break
+# #     so = so - 1
+
+# # while True:
+# #     if n%so == 0 and so%2 != 0:
+# #         print(so)
+# #         break
+# #     so = so - 1
+
+
+# # so = n//2
+# # while not(n%so==0 and so%2!=0):
+# #     so=so-1
+
+
+n=int(input('Nhap n: '))
+S=int(input('Nhap S: '))
+so=1
+tong=0
+while so<=n:
+    tong=tong+so
+    so=so+1
+if tong-S==0:
+    print('Bo ve du')
+else:
+    print('So thu tu con bi mat: {}'.format(tong-S))
