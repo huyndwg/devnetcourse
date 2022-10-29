84d222d2496c56ae4f632f3d5b47e5f3cea7271b
huyndwg <huy.ndwg@gmail.com>
2022-10-29 15:37:26 +0700

    ok zoi day

--- oct28/deviceinfo.py
+++ oct28/deviceinfo.py
@@ -4,17 +4,18 @@ from tabulate import tabulate
 
 def main():
     # Get device info
-    cookies = Sd_wan.get_cookies(end_point="", username="", password="")
+    cookies = Sd_wan.get_cookies(end_point="/j_security_check", username="devnetuser", password="RG!_Yw919_83")
     token = Sd_wan.get_token(jsessionid=cookies)
-    device = Sd_wan.get_device(end_point="", jsesssionid=cookies, token=token)
+    device = Sd_wan.get_device(end_point="/device", jsesssionid=cookies, token=token)
     # Format device info
     headers_table = ["Hostname", "Device", "UUID", "Status"]
     table_data = []
-    for item in device:
+    for item in device["data"]:
         row = [item["host-name"], item["deviceId"], item["uuid"], item["status"]]
-        table_data.append[row]
+        row = [item["host-name"], item["deviceId"], item["uuid"], item["status"]]
+        table_data.append(row)
     # Print device info
-    table = tabulate(table_data,headers=headers_table,tablefmt="github")
+    table = tabulate(table_data, headers=headers_table, tablefmt="github")
     print(table)
 
 
