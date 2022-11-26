77c1caa521ec8616acb4ff22db90e94ed7d730ac
Huy Dang Nguyen <huy.ndwg@gmail.com>
2022-11-26 14:51:51 +0700

    update

--- pyw2_14.py
+++
@@ -1,9 +0,0 @@
-ttl=int(input('Nhap so tien: '))
-type=[500,200,100,50,20,10,5,2,1]
-sum = 0
-print('So tien {} se duoc doi thanh:'.format(ttl))
-for i in type:
-    print('Loai {} gom {} to'.format(i,ttl//i))
-    sum = sum + ttl//i
-    ttl = ttl%i
-print('TONG CONG CO {} TO'.format(sum))
