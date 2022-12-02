291d6a09febfbd878da3c6fe925a743e1027fe34
Huy Dang Nguyen <huy.ndwg@gmail.com>
2022-12-02 21:01:51 +0700

    update

--- dec2/api.py
+++ dec2/api.py
@@ -1,4 +1,4 @@
-from flask import request
+import requests
 
 access_token = "MmY0MGUwNzEtOTE3MS00MDRhLTkyYzEtYTI5NDZhNzRmZmM5YmExZDA2YWYtNjZk_P0A1_caac8e9f-ddc7-4e4f-ab82-d031172a892e"
 
@@ -13,5 +13,24 @@ headers = {
 
 
 
-def get_messages():
+def get_messages(messageId):
     url = f"{base_url}/v1/messages/{message_id}"
+
+    response = requests.get(url=url, headers=headers)
+
+    return response
+
+def post_messages(roomId, text_msg):
+    url = f"{base_url}/v1/messages/"
+
+    body = {
+        "roomId":roomId,
+        "text" : text_msg
+    }
+
+    response = requests.post(url=url, headers=headers, json=body)
+
+    print(response.status_code)
+
+if __name__ == "__main__":
+    print(get_messages(messageId=message_id))
--- dec2/webex.py
+++ dec2/webex.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+from api import get_messages, post_messages
+import json
 
 app = Flask(__name__)
 
@@ -8,7 +10,16 @@ def index():
         return "Helo ong gia ngheo kho"
     elif request.method == "POST":
         data = request.get_json()
-        print(data)
+        roomId = data['data']['roomId']
+        msgId = data['data']['id']
+        print(json.dumps(data, indent=3))
+        print(roomId)
+
+        usr_msg = get_messages(msgId)
+        print(usr_msg)
+
+        bot_send_msg = f"Message sent: {usr_msg}"
+        post_messages(roomId=roomId, text_msg=usr_msg)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6969)
