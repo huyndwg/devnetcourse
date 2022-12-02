992b13197947ffa5c5db20d49826b05c237b04f3
Huy Dang Nguyen <huy.ndwg@gmail.com>
2022-12-02 20:37:48 +0700

    update

--- dec2/webex.py
+++ dec2/webex.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "App run on port 6969"
+    if request.method == "GET":
+        return "Helo ong gia ngheo kho"
+    elif request.method == "POST":
+        data = request.get_json()
+        print(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6969)
